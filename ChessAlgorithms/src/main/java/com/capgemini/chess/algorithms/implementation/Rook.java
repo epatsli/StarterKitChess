19559bfe0235a28a40ff06e369186ae68517fc33
epatsli <PATRSLIW@CE24519.corp.capgemini.com>
2018-07-17 09:44:46 +0200

    Validate move for rook and my test.

--- ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/Rook.java
+++ ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/Rook.java
@@ -9,10 +9,11 @@ import com.capgemini.chess.algorithms.data.enums.Piece;
 import com.capgemini.chess.algorithms.data.generated.Board;
 import com.capgemini.chess.algorithms.implementation.exceptions.InvalidMoveException;
 
-public class Rook implements PiecesMove{
+public class Rook implements PiecesMove {
 
 	// @Override
-	public boolean checkMove(Board board, Coordinate coordinateFrom, Coordinate coordinateTo) throws InvalidMoveException {
+	public boolean checkMove(Board board, Coordinate coordinateFrom, Coordinate coordinateTo)
+			throws InvalidMoveException {
 
 		int coordinateFromX = coordinateFrom.getX();
 		int coordinateFromY = coordinateFrom.getY();
@@ -22,12 +23,16 @@ public class Rook implements PiecesMove{
 		CheckData check = new CheckData();
 		List<Coordinate> listMoveRook = new ArrayList<>();
 
-		if (((coordinateFromX == coordinateToX) || (coordinateFromY == coordinateToY))&&(checkFieldBetweenFromAndTo(board, coordinateFrom, coordinateTo)))
+		if (((coordinateFromX == coordinateToX) || (coordinateFromY == coordinateToY))
+				&& (checkFieldBetweenFromAndTo(board, coordinateFrom, coordinateTo))
+				&& ((checkFieldToisEmpty(board, coordinateTo))
+						|| (checkEqualColorPlayerFromAndTo(board, coordinateFrom, coordinateTo))))
 			return true;
 		return false;
 	}
-	
-	private boolean checkFieldBetweenFromAndTo(Board board, Coordinate coordinateFrom, Coordinate coordinateTo) throws InvalidMoveException {
+
+	private boolean checkFieldBetweenFromAndTo(Board board, Coordinate coordinateFrom, Coordinate coordinateTo)
+			throws InvalidMoveException {
 
 		int coordinateFromX = coordinateFrom.getX();
 		int coordinateFromY = coordinateFrom.getY();
@@ -41,47 +46,69 @@ public class Rook implements PiecesMove{
 		int differentCoordinateY = Math.abs(coordinateFromY - coordinateToY);
 
 		Piece pieceBetween;
+		if ((differentCoordinateY == 1) || (differentCoordinateX == 1))
+			return true;
+		else {
+			if ((coordinateFromY > coordinateToY) && (coordinateFromX == coordinateToX)) {
 
-		if ((coordinateFromY > coordinateToY) && (coordinateFromX == coordinateToX)) {
+				for (int i = 1; i < differentCoordinateY; i++) {
+					coordinateBetweenY = new Coordinate(coordinateFromX, coordinateFromY - i);
+					pieceBetween = board.getPieceAt(coordinateBetweenY);
+					if (pieceBetween == null)
+						return true;
+					else
+						throw new InvalidMoveException();
 
-			for (int i = 1; i < differentCoordinateY; i++) {
-				coordinateBetweenY = new Coordinate(coordinateFromX, coordinateFromY - i);
-				pieceBetween = board.getPieceAt(coordinateBetweenY);
-				if (pieceBetween == null)
-					return true;
-				else
-					throw new InvalidMoveException();
-
-			}
-		} else if ((coordinateToY > coordinateFromY) && (coordinateFromX == coordinateToX)) {
-			for (int i = 1; i < differentCoordinateY; i++) {
-				coordinateBetweenY = new Coordinate(coordinateFromX, coordinateFromY + i);
-				pieceBetween = board.getPieceAt(coordinateBetweenY);
-				if (pieceBetween == null)
-					return true;
-				else
-					throw new InvalidMoveException();
-			}
-		} else if ((coordinateFromX > coordinateToX) && (coordinateFromY == coordinateToY)) {
-			for (int i = 1; i < differentCoordinateX; i++) {
-				coordinateBetweenX = new Coordinate(coordinateFromX - i, coordinateFromY);
-				pieceBetween = board.getPieceAt(coordinateBetweenX);
-				if (pieceBetween == null)
-					return true;
-				else
-					throw new InvalidMoveException();
-			}
-		} else if ((coordinateToX > coordinateFromX) && (coordinateFromY == coordinateToY)) {
-			for (int i = 1; i < differentCoordinateX; i++) {
-				coordinateBetweenX = new Coordinate(coordinateFromX + i, coordinateFromY);
-				pieceBetween = board.getPieceAt(coordinateBetweenX);
-				if (pieceBetween == null)
-					return true;
-				else
-					throw new InvalidMoveException();
+				}
+			} else if ((coordinateToY > coordinateFromY) && (coordinateFromX == coordinateToX)) {
+				for (int i = 1; i < differentCoordinateY; i++) {
+					coordinateBetweenY = new Coordinate(coordinateFromX, coordinateFromY + i);
+					pieceBetween = board.getPieceAt(coordinateBetweenY);
+					if (pieceBetween == null)
+						return true;
+					else
+						throw new InvalidMoveException();
+				}
+			} else if ((coordinateFromX > coordinateToX) && (coordinateFromY == coordinateToY)) {
+				for (int i = 1; i < differentCoordinateX; i++) {
+					coordinateBetweenX = new Coordinate(coordinateFromX - i, coordinateFromY);
+					pieceBetween = board.getPieceAt(coordinateBetweenX);
+					if (pieceBetween == null)
+						return true;
+					else
+						throw new InvalidMoveException();
+				}
+			} else if ((coordinateToX > coordinateFromX) && (coordinateFromY == coordinateToY)) {
+				for (int i = 1; i < differentCoordinateX; i++) {
+					coordinateBetweenX = new Coordinate(coordinateFromX + i, coordinateFromY);
+					pieceBetween = board.getPieceAt(coordinateBetweenX);
+					if (pieceBetween == null)
+						return true;
+					else
+						throw new InvalidMoveException();
+				}
 			}
 		}
 		return false;
 	}
 
+	private boolean checkEqualColorPlayerFromAndTo(Board board, Coordinate coordinateFrom, Coordinate coordinateTo) {
+
+		Piece piece1 = board.getPieceAt(coordinateFrom);
+		Piece piece2 = board.getPieceAt(coordinateTo);
+
+		if ((piece1.getColor()) != (piece2.getColor()))
+			return true;
+
+		return false;
+	}
+
+	private boolean checkFieldToisEmpty(Board board, Coordinate coordinateTo) {
+
+		Piece piece = board.getPieceAt(coordinateTo);
+		if (piece == null)
+			return true;
+
+		return false;
+	}
 }
---
+++ ChessAlgorithms/src/main/java/myTest/RookTest.java
@@ -0,0 +1,177 @@
+package myTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.capgemini.chess.algorithms.data.Coordinate;
+import com.capgemini.chess.algorithms.data.Move;
+import com.capgemini.chess.algorithms.data.enums.MoveType;
+import com.capgemini.chess.algorithms.data.enums.Piece;
+import com.capgemini.chess.algorithms.data.generated.Board;
+import com.capgemini.chess.algorithms.implementation.BoardManager;
+import com.capgemini.chess.algorithms.implementation.Rook;
+import com.capgemini.chess.algorithms.implementation.exceptions.InvalidMoveException;
+
+public class RookTest {
+
+	@Test
+	public void shouldCheckMoveForRook() throws InvalidMoveException {
+
+
+		// given
+		Rook r = new Rook();
+		Board board = new Board();
+		Coordinate coordinateWhiteRook = new Coordinate(0, 0);
+		Coordinate coordinateBlackRook = new Coordinate(7, 7);
+		board.setPieceAt(Piece.WHITE_ROOK, coordinateWhiteRook);
+		board.setPieceAt(Piece.BLACK_ROOK, coordinateBlackRook);
+
+		// when
+		BoardManager boardManager = new BoardManager(board);
+
+		// then
+		assertEquals(true, r.checkMove(board, coordinateWhiteRook, new Coordinate(0, 7)));
+		assertEquals(true, r.checkMove(board, coordinateWhiteRook, new Coordinate(7, 0)));
+		assertEquals(false, r.checkMove(board, coordinateWhiteRook, new Coordinate(4, 1)));
+		assertEquals(false, r.checkMove(board, coordinateWhiteRook, new Coordinate(5, 3)));
+		assertEquals(false, r.checkMove(board, coordinateWhiteRook, new Coordinate(7, 7)));
+
+		assertEquals(true, r.checkMove(board, coordinateBlackRook, new Coordinate(3, 7)));
+		assertEquals(true, r.checkMove(board, coordinateBlackRook, new Coordinate(7, 4)));
+		assertEquals(false, r.checkMove(board, coordinateBlackRook, new Coordinate(0, 4)));
+		assertEquals(false, r.checkMove(board, coordinateBlackRook, new Coordinate(5, 3)));
+		assertEquals(false, r.checkMove(board, coordinateBlackRook, new Coordinate(0, 6)));
+	}
+
+	@Test (expected = InvalidMoveException.class)
+	public void shouldCheckMoveForRookByPiece() throws InvalidMoveException {
+		
+		// given
+		Rook r=new Rook();
+		Coordinate coordinateWhiteRook=new Coordinate(0, 0);
+		Board board = new Board();
+		
+		// when
+		board.setPieceAt(Piece.BLACK_ROOK, new Coordinate(0, 7));
+		board.setPieceAt(Piece.BLACK_KNIGHT, new Coordinate(1, 7));
+		board.setPieceAt(Piece.BLACK_BISHOP, new Coordinate(2, 7));
+		board.setPieceAt(Piece.BLACK_QUEEN, new Coordinate(3, 7));
+		board.setPieceAt(Piece.BLACK_KING, new Coordinate(4, 7));
+		board.setPieceAt(Piece.BLACK_BISHOP, new Coordinate(5, 7));
+		board.setPieceAt(Piece.BLACK_KNIGHT, new Coordinate(6, 7));
+		board.setPieceAt(Piece.BLACK_ROOK, new Coordinate(7, 7));
+
+		for (int x = 0; x < Board.SIZE; x++) {
+			board.setPieceAt(Piece.BLACK_PAWN, new Coordinate(x, 6));
+		}
+
+		board.setPieceAt(Piece.WHITE_ROOK, new Coordinate(0, 0));
+		board.setPieceAt(Piece.WHITE_KNIGHT, new Coordinate(1, 0));
+		board.setPieceAt(Piece.WHITE_BISHOP, new Coordinate(2, 0));
+		board.setPieceAt(Piece.WHITE_QUEEN, new Coordinate(3, 0));
+		board.setPieceAt(Piece.WHITE_KING, new Coordinate(4, 0));
+		board.setPieceAt(Piece.WHITE_BISHOP, new Coordinate(5, 0));
+		board.setPieceAt(Piece.WHITE_KNIGHT, new Coordinate(6, 0));
+		board.setPieceAt(Piece.WHITE_ROOK, new Coordinate(7, 0));
+
+		for (int x = 0; x < Board.SIZE; x++) {
+			board.setPieceAt(Piece.WHITE_PAWN, new Coordinate(x, 1));
+		}
+		
+		// then
+		assertEquals(false,  r.checkMove(board, coordinateWhiteRook, new Coordinate(0, 7)));
+		
+	}
+	
+	@Test
+	public void shouldCheckMoveForRookOnChessboardCapture() throws InvalidMoveException {
+		
+		// given
+		Rook r=new Rook();
+		Coordinate coordinateWhiteRook=new Coordinate(0, 2);
+		Board board = new Board();
+		
+		// when
+		board.setPieceAt(Piece.BLACK_ROOK, new Coordinate(0, 7));
+		board.setPieceAt(Piece.BLACK_KNIGHT, new Coordinate(1, 7));
+		board.setPieceAt(Piece.BLACK_BISHOP, new Coordinate(2, 7));
+		board.setPieceAt(Piece.BLACK_QUEEN, new Coordinate(3, 7));
+		board.setPieceAt(Piece.BLACK_KING, new Coordinate(4, 7));
+		board.setPieceAt(Piece.BLACK_BISHOP, new Coordinate(5, 7));
+		board.setPieceAt(Piece.BLACK_KNIGHT, new Coordinate(6, 7));
+		board.setPieceAt(Piece.BLACK_ROOK, new Coordinate(7, 7));
+
+		for (int x = 0; x < Board.SIZE; x++) {
+			board.setPieceAt(Piece.BLACK_PAWN, new Coordinate(x, 6));
+		}
+
+		board.setPieceAt(Piece.WHITE_ROOK, coordinateWhiteRook);
+		board.setPieceAt(Piece.WHITE_KNIGHT, new Coordinate(1, 0));
+		board.setPieceAt(Piece.WHITE_BISHOP, new Coordinate(2, 0));
+		board.setPieceAt(Piece.WHITE_QUEEN, new Coordinate(3, 0));
+		board.setPieceAt(Piece.WHITE_KING, new Coordinate(4, 0));
+		board.setPieceAt(Piece.WHITE_BISHOP, new Coordinate(5, 0));
+		board.setPieceAt(Piece.WHITE_KNIGHT, new Coordinate(6, 0));
+		board.setPieceAt(Piece.WHITE_ROOK, new Coordinate(7, 0));
+
+		for (int x = 0; x < Board.SIZE; x++) {
+			board.setPieceAt(Piece.WHITE_PAWN, new Coordinate(x, 1));
+		}
+		
+		// then
+		assertEquals(true,  r.checkMove(board, coordinateWhiteRook, new Coordinate(0, 5)));
+		assertEquals(true,  r.checkMove(board, new Coordinate(0, 5), new Coordinate(5, 5)));
+		board.setPieceAt(Piece.WHITE_ROOK, new Coordinate(5, 5));
+		assertEquals(true,  r.checkMove(board, new Coordinate(5, 5), new Coordinate(5, 6)));
+		assertEquals(true,  r.checkMove(board, new Coordinate(5, 5), new Coordinate(5, 3)));
+		board.setPieceAt(Piece.WHITE_ROOK, new Coordinate(5, 3));
+		assertEquals(false,  r.checkMove(board, new Coordinate(5, 3), new Coordinate(5, 1)));
+	}
+	
+	@Test (expected = InvalidMoveException.class)
+	public void shouldCheckMoveForRookOnChessboardByPiece() throws InvalidMoveException {
+		
+		// given
+		Rook r=new Rook();
+		Coordinate coordinateWhiteRook=new Coordinate(0, 0);
+		Board board = new Board();
+		
+		// when
+		board.setPieceAt(Piece.BLACK_ROOK, new Coordinate(0, 7));
+		board.setPieceAt(Piece.BLACK_KNIGHT, new Coordinate(1, 7));
+		board.setPieceAt(Piece.BLACK_BISHOP, new Coordinate(2, 7));
+		board.setPieceAt(Piece.BLACK_QUEEN, new Coordinate(3, 7));
+		board.setPieceAt(Piece.BLACK_KING, new Coordinate(4, 7));
+		board.setPieceAt(Piece.BLACK_BISHOP, new Coordinate(5, 7));
+		board.setPieceAt(Piece.BLACK_KNIGHT, new Coordinate(6, 7));
+		board.setPieceAt(Piece.BLACK_ROOK, new Coordinate(7, 7));
+
+		for (int x = 0; x < Board.SIZE; x++) {
+			board.setPieceAt(Piece.BLACK_PAWN, new Coordinate(x, 6));
+		}
+
+		board.setPieceAt(Piece.WHITE_ROOK, coordinateWhiteRook);
+		board.setPieceAt(Piece.WHITE_KNIGHT, new Coordinate(1, 0));
+		board.setPieceAt(Piece.WHITE_BISHOP, new Coordinate(2, 0));
+		board.setPieceAt(Piece.WHITE_QUEEN, new Coordinate(3, 0));
+		board.setPieceAt(Piece.WHITE_KING, new Coordinate(4, 0));
+		board.setPieceAt(Piece.WHITE_BISHOP, new Coordinate(5, 0));
+		board.setPieceAt(Piece.WHITE_KNIGHT, new Coordinate(6, 0));
+		board.setPieceAt(Piece.WHITE_ROOK, new Coordinate(7, 0));
+
+		for (int x = 0; x < Board.SIZE; x++) {
+			board.setPieceAt(Piece.WHITE_PAWN, new Coordinate(x, 1));
+		}
+		
+		// then
+		board.setPieceAt(Piece.BLACK_PAWN, new Coordinate(0, 3));
+		assertEquals(true,  r.checkMove(board, new Coordinate(0, 2), new Coordinate(0, 4)));
+
+	}
+}
